f41cfe7e6d64402198c02b6be93e7c6395f0f1d5
Jamir Mansilla <davidjamirmansilla@gmail.com>
2021-10-18 05:34:00 -0300

    commit with the styles of the formulary done, rest to make it responsive and add the js validation

---
+++ JsFiles/js.js
@@ -0,0 +1,14 @@
+/*Get all the element by their Id*/
+
+var name = document.getElementById("name");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var age = document.getElementById("age");
+var phone = document.getElementById("phone");
+var address = document.getElementById("address");
+var city = document.getElementById("city");
+var postalCode = document.getElementById("postalCode");
+var id = document.getElementById("id");
+
+
+
